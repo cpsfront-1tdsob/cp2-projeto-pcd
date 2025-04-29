6624fecb6c0646cfcb86ca41706ae0eb956c7917
brucantacini <brunocantacini.ti@gmail.com>
2025-04-29 09:12:23 -0300

    Criando a nav bar e seus botoes.

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -29,6 +29,10 @@ const Nav = () => {
                 )
             })}
             </div>
+            {/* {Buttons} */}
+            <div className="flex items-center space-x-4">
+                <button className='md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transiction-all duration-200 rounded-lg'>Entrar</button>
+            </div>
         </div>
     </div>
   )
