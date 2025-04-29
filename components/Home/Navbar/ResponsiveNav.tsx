97412581d397a64d9e6622ab1d1fc01e099b65e5
brucantacini <brunocantacini.ti@gmail.com>
2025-04-29 09:51:18 -0300

    Usando hooks, convertendo componentes de server em client.

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,18 @@
+'use client'
 import React from 'react'
 import MobileNav from './MobileNav'
 import Nav from './Nav'
 
 const ResponsiveNav = () => {
+  // para usar hooks é necessario converter os componentes para client components
+  // o que é feito com o 'use client' no topo do arquivo
+  const [showNav, setshowNav] = React.useState(false);
+  const handNavShow = () => setshowNav(true);
+  const handleCloseNav = () => setshowNav(false);
   return (
     <div>
-        <Nav/>
-        <MobileNav/>
+        <Nav openNav={handNavShow}/>
+        <MobileNav showNav={showNav} closeNav={handleCloseNav}/>
     </div>
   )
 }
