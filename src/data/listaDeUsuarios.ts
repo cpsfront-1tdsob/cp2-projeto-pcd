095c03e6dced4e42df231c75db79e38649d7149d
amandaagaldino <amandagaldinox@gmail.com>
2025-05-04 21:14:35 -0300

    arrumando a lista de usuarios

--- src/data/listaDeUsuarios.ts
+++ src/data/listaDeUsuarios.ts
@@ -1,7 +1,7 @@
 import { TipoUsuario } from "@/types/TipoUsuario";
 
 export const listaDeUsuarios:TipoUsuario[] = [
-    {id: 1,nome: "Amanda",email:"amanda@email.com",senha:"amanda123"},
-    {id: 2,nome: "Bruno",email:"bruno@email.com",senha:"bruno123"},
-    {id: 3,nome: "Gustavo",email:"gustavo@email.com",senha:"gustavo123"}
+    {id: 1,nome: "Amanda",email:"amanda@gmail.com",senha:"amanda123"},
+    {id: 2,nome: "Bruno",email:"bruno@gmail.com",senha:"bruno123"},
+    {id: 3,nome: "Gustavo",email:"gustavo@gmail.com",senha:"gustavo123"}
 ];
