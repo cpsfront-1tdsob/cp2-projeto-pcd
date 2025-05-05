e18bc35989241b1ada6080fc091b636eb044c71e
amandaagaldino <amandagaldinox@gmail.com>
2025-05-04 21:13:09 -0300

    arrumando os nomes da imagens e suas rotas

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -2,42 +2,42 @@
 export const serivcesData = [
     {
       id: 1,
-      image: "../public/images/Serviços-de-Saúde-Reabilitação.jpg",
+      image: "/images/Servicos-de-Saude-Reabilitacao.jpg",
       tipo: "Saúde e Reabilitação"
     },
     {
       id: 2,
-      image: "../public/images/Benefícios-Direito.webp",
+      image: "/images/Beneficios-Direito.webp",
       tipo: "Benefícios e Direitos"
     },
     {
       id: 3,
-      image: "../public/images/Emprego-Profissionalização.jpg",
+      image: "/images/Emprego-Profissionalizacao.jpg",
       tipo: "Emprego e profissionalização"
     },
     {
       id: 4,
-      image: "../public/images/Educação-Inclusiva.jpg",
+      image: "/images/Educacao-Inclusiva.jpg",
       tipo: "Educação Inclusiva"
     },
     {
       id: 5,
-      image: "../public/images/Acessibilidade-Urbana-Mobilidade.png",
+      image: "/images/Acessibilidade-Urbana-Mobilidade.png",
       tipo: "Acessibilidade urbana"
     },
     {
       id: 6,
-      image: "../public/images/Comunidades-Apoio-Psicológico.webp",
+      image: "/images/Comunidades-Apoio-Psicologico.webp",
       tipo: "Apoio psicológico"
     },
     {
       id: 7,
-      image: "../public/images/Produtos-Tecnologia-Assistiva.png",
+      image: "/images/Produtos-Tecnologia-Assistiva.png",
       tipo: "Produtos Tecnologia Assistiva"
     },
     {
       id: 8,
-      image: "../public/images/Denúncias-Reclamações.jpg",
+      image: "/images/Denuncias-Reclamacoes.jpg",
       tipo: "Denúncias e Reclamações"
     },
   ];
